f9e2ab527c9d96e538d3e6897f6d9132ce4ef76c
BrandonLeeDotDev <72579318+BrandonLeeDotDev@users.noreply.github.com>
2020-11-15 16:34:43 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -9,6 +9,7 @@ fn main() {
     let mut vec = vec![0,0,0,0,0,0,0,0,0];
 
     loop {
+        // --------- v Necessary for Algo v --------- //
         fn time(switch: bool) -> u128 {
             let mut time = 0;
             if switch == true {
@@ -52,6 +53,10 @@ fn main() {
         }
 
         let num = ((reversed as f64 * 0.0000000000000001) + 1.0).powf(pow).trunc() as u128;
+        
+        // --------- ^ Necessary for Algo ^ --------- //
+        
+        // -------------- V For Stats v ------------- //
 
         let num_length = num.to_string().chars().count() as u8;
 
@@ -72,11 +77,16 @@ fn main() {
 
         println!("1x {:.2}%, 2x {:.2}%. 3x {:.2}%, 4x {:.2}%, 5x {:.2}%, 6x {:.2}%, 7x {:.2}%, 8x {:.2}%, 9x {:.2}% -- Iterations:{} - Digits:{}{} -- Rand:{}", number(vec[0], count), number(vec[1], count), number(vec[2], count), number(vec[3], count), number(vec[4], count), number(vec[5], count), number(vec[6], count), number(vec[7], count), number(vec[8], count),count,space,num_length, num);
 
+        count += 1;
+        
+        // -------------- ^ For Stats ^ ------------- //
+        
+        // -------------- v Max Limit v ------------- //
+        
         if num == 340282366920938463463374607431768211455 {
             println!("Error Max : {}", num);
             break;
         }
 
-        count += 1;
     }
 }
