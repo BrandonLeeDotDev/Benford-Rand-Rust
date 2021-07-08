d84dd067c361c2feb9977aa192eee6938bc4619e
BrandonLeeDotDev <72579318+BrandonLeeDotDev@users.noreply.github.com>
2021-07-07 19:49:20 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,60 +1,43 @@
 use std::{
-    time::{Duration, SystemTime, UNIX_EPOCH},
-    thread,
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 fn main() {
-    let mut count = 1;
 
-    let mut vec = vec![0,0,0,0,0,0,0,0,0];
+    let mut vec = vec![10;0];
+
+    let mut sum = 0.0;
 
     loop {
-        // --------- v Necessary for Algo v --------- //
-        fn time(switch: bool) -> u128 {
-            if switch == true {
-                SystemTime::now()
+        vec[9] = vec_update(vec[9]);
+
+        let count = vec[9];
+
+        fn time() -> u128 {
+            SystemTime::now()
                 .duration_since(UNIX_EPOCH)
                 .expect("Before time!")
                 .as_nanos()
-            } else {
-                SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .expect("Before time!")
-                .as_millis()
-            }
         }
 
-        let start_time = time(true);
+        let mut start_time = time();
 
-        thread::sleep(Duration::from_nanos(1));
+        let pow = ((time() % 89) + 10) as f64 * 0.1;
 
-        let mut loop_time = (time(true) % 89) + 10;
+        sum += pow;
 
-        let pow = loop_time as f64 * 0.1;
-
-        while loop_time > 0 {
-            loop_time *= loop_time;
-            thread::sleep(Duration::from_nanos(1));
-            loop_time /= loop_time;
-            loop_time -= 1;
-        }
-
-        let end_time = time(false);
+        let mean = sum / count as f64;
 
         let mut reversed = 0;
 
-        let mut calc_time = start_time - end_time;
-
-        while calc_time != 0 {
-            reversed = reversed * 10 + calc_time % 10;
-            calc_time /= 10;
+        while start_time != 0 {
+            reversed = reversed * 10 + start_time % 10;
+            start_time /= 10;
         }
 
-        let num = ((reversed as f64 * 0.0000000000000001) + 1.0).powf(pow).trunc() as u128;
-        
-        // --------- ^ Necessary for Algo ^ --------- //
-        
-        // -------------- V For Stats v ------------- //
+        //let num = ((reversed as f64 * (mean * 0.000000000000000149)) + 1.0).powf(pow).trunc() as u128; //0.203
+
+        let num = ((reversed as f64).powf(pow) + reversed as f64) * 0.000000000000001;
 
         let num_length = num.to_string().chars().count() as u8;
 
@@ -73,16 +56,12 @@ fn main() {
 
         let space = if num_length >= 1 && num_length <= 9 {"0"} else {""};
 
-        println!("1x {:.2}%, 2x {:.2}%. 3x {:.2}%, 4x {:.2}%, 5x {:.2}%, 6x {:.2}%, 7x {:.2}%, 8x {:.2}%, 9x {:.2}% -- Iterations:{} - Digits:{}{} -- Rand:{}", number(vec[0], count), number(vec[1], count), number(vec[2], count), number(vec[3], count), number(vec[4], count), number(vec[5], count), number(vec[6], count), number(vec[7], count), number(vec[8], count),count,space,num_length, num);
+        println!("1x {:.2}%, 2x {:.2}%. 3x {:.2}%, 4x {:.2}%, 5x {:.2}%, 6x {:.2}%, 7x {:.2}%, 8x {:.2}%, 9x {:.2}% - Iterations:{} - Digits:{}{} -- Rand:{} ", number(vec[0], count), number(vec[1], count), number(vec[2], count), number(vec[3], count), number(vec[4], count), number(vec[5], count), number(vec[6], count), number(vec[7], count), number(vec[8], count),count, space,num_length, num);
 
-        count += 1;
-        
-        // -------------- ^ For Stats ^ ------------- //
-        
-        // -------------- v Max Limit v ------------- //
-        
-        if num == 340282366920938463463374607431768211455 {
-            println!("Error Max : {}", num);
+        //println!("Iterations:{} - Digits:{}{} - Rand:{} - Start:{}",count, space,num_length, num, reversed);
+
+        if num == 340282366920938463463374607431768211455.0 {
+            println!("Found : {}", num);
             break;
         }
 
